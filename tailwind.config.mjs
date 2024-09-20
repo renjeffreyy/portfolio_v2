48c4cbf161d74a5c3e13129ba94133f320a139cb
Jeffrey Ren <renjeffreyy@gmail.com>
2024-09-20 14:14:35 -0700

    finished moile header

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		colors:{
+			'blue-900': '#10172a',
+			'blue-800': '#13315C',
+			'blue-700': '#134074',
+			'blue-600': '#8DA9C4',
+			'blue-100': '#EEF4ED'
+		},
+		extend: {},
+	},
+	plugins: [],
+}
