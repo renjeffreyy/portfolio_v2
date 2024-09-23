86384b03aa26092a090ae7f435f64f06f8024470
Jeffrey Ren <renjeffreyy@gmail.com>
2024-09-23 15:08:06 -0700

    finished basic experience layout

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,14 +2,26 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		colors:{
-			'blue-900': '#10172a',
-			'blue-800': '#13315C',
-			'blue-700': '#134074',
-			'blue-600': '#8DA9C4',
-			'blue-100': '#EEF4ED'
+		extend: {
+			colors:{
+				'blue-900': '#10172a',
+				'blue-800': '#13315C',
+				'blue-700': '#134074',
+				'blue-600': '#8DA9C4',
+				'blue-100': '#EEF4ED',
+			},
 		},
-		extend: {},
 	},
-	plugins: [],
+	plugins: [
+		function ({ addComponents, theme }) {
+			addComponents({
+			  '.text-color': {
+				color: theme('colors.blue-600'),
+			  },
+			  '.header-color': {
+				color: theme('colors.blue-100')
+			  }
+			});
+		}
+	],
 }
